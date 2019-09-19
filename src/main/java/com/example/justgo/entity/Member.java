9e981f5ac5efa24ff666c661d6269a7d9b4b9ac1
YANYUFENG <LalicID@outlook.com>
2019-09-19 13:33:20 +0800

    :art: add jpa

--- src/main/java/com/example/justgo/entity/Member.java
+++ src/main/java/com/example/justgo/entity/Member.java
@@ -1,21 +1,28 @@
-package com.example.justgo.pojo;
+package com.example.justgo.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 /**
  * @auth chejiahui
  * @date 2019/9/18 18:02
  */
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "member")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String mid;
     private String realName;
     private Timestamp createTime;
---
+++ src/main/java/com/example/justgo/util/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.justgo.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @Auther: YYF
+ * @Date: 2019/9/19 11:58
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+}
